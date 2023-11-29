0ccc5f6fa9486c7e9943c33e6b725ab6bd6da5ff
HarshanaSenadeera <harshanasenadeera64@gmail.com>
2023-11-29 20:00:49 +0530

    Create Bubble short main class and sub clas

---
+++ src/lk/ijse/algorithms/bubbleShort/BubbleShort.java
@@ -0,0 +1,4 @@
+package lk.ijse.algorithms.bubbleShort;
+
+public class BubbleShort {
+}
---
+++ src/lk/ijse/algorithms/bubbleShort/BubbleShortMain.java
@@ -0,0 +1,8 @@
+package lk.ijse.algorithms.bubbleShort;
+
+public class BubbleShortMain {
+
+    public static void main(String[] args) {
+
+    }
+}
