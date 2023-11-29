1db3e25a9e804cee8237c9b86bf9ba2cd5ff56fa
HarshanaSenadeera <harshanasenadeera64@gmail.com>
2023-11-29 20:20:18 +0530

    call search method and run program  and modify code

--- src/lk/ijse/algorithms/bubbleShort/BubbleShort.java
+++ src/lk/ijse/algorithms/bubbleShort/BubbleShort.java
@@ -1,14 +1,18 @@
 package lk.ijse.algorithms.bubbleShort;
 
+import java.util.Arrays;
+
 public class BubbleShort {
 
     //(01)create method
-    //(02)pass int array and target as parameters
+    //(02)pass int array parameters
     //(03)check first two elements which grater than next element
     //(04)create variable temp in int
     //(05)then swap
-    public int search(int [] arr,int target){
+    public int search(int [] arr){
 
+        System.out.println("===================Before shorting array=================");
+        System.out.println(Arrays.toString(arr));
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
 
@@ -21,6 +25,8 @@ public class BubbleShort {
                 }
             }
         }
+        System.out.println("===================After shorting array=================");
+        System.out.println(Arrays.toString(arr));
         return -1;
     }
 }
--- src/lk/ijse/algorithms/bubbleShort/BubbleShortMain.java
+++ src/lk/ijse/algorithms/bubbleShort/BubbleShortMain.java
@@ -1,11 +1,15 @@
 package lk.ijse.algorithms.bubbleShort;
 
+import java.util.Arrays;
+
 public class BubbleShortMain {
 
     public static void main(String[] args) {
 
+       int [] arr={100,40,80,10,30,20,50,70,90,60};
+
         BubbleShort bubbleShort = new BubbleShort();
 
-        bubbleShort.search();
+        bubbleShort.search(arr);
     }
 }
