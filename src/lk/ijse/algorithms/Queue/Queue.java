501fb4a27e0a83cd9715ad16866659e96a356719
HarshanaSenadeera <harshanasenadeera64@gmail.com>
2023-12-01 10:02:32 +0530

    create variables and declare

--- src/lk/ijse/algorithms/Queue/Queue.java
+++ src/lk/ijse/algorithms/Queue/Queue.java
@@ -1,4 +1,18 @@
 package lk.ijse.algorithms.Queue;
 
 public class Queue {
+
+    int start;
+    int rear;
+    int index;
+    int [] arr;
+
+    public Queue(int size){
+        arr=new int[size];
+        start =0;
+        rear =-1;
+        index=0;
+    }
+
+
 }
