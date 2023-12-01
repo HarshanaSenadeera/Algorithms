28406d6ee71778bd328d2344a83ba3f68af492e4
HarshanaSenadeera <harshanasenadeera64@gmail.com>
2023-12-01 10:15:50 +0530

    Create enQueue method to insert values to the queue

--- src/lk/ijse/algorithms/Queue/Queue.java
+++ src/lk/ijse/algorithms/Queue/Queue.java
@@ -2,17 +2,30 @@ package lk.ijse.algorithms.Queue;
 
 public class Queue {
 
-    int start;
+    int front;
     int rear;
     int index;
     int [] arr;
 
     public Queue(int size){
         arr=new int[size];
-        start =0;
+        front =0;
         rear =-1;
         index=0;
     }
+
+    public void enQueue(int data){
+        if (index == arr.length){
+            System.out.println("Queue is full...!");
+        }
+        if (index == arr.length-1){
+            rear = -1;
+        }
+        arr[++rear]=data;
+        index++;
+    }
+
+
 
 
 }
