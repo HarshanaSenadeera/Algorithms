a3ea9af7182e6ff65609592b80e8218437a30cd9
HarshanaSenadeera <harshanasenadeera64@gmail.com>
2023-12-02 20:43:26 +0530

    create inner class Node and create data and link as Node's properties and pass these properties via constructor

--- src/lk/ijse/algorithms/LinkedList/LinkedList.java
+++ src/lk/ijse/algorithms/LinkedList/LinkedList.java
@@ -3,4 +3,16 @@ package lk.ijse.algorithms.LinkedList;
 public class LinkedList {
 
 
+    private static class Node{
+
+       private int data;
+       private Node link;
+
+       public Node(int data){
+           this.data=data;
+           this.link=null;
+       }
+    }
+
+
 }
